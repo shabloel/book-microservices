dc7e5b20b83f6df45c0ecc60bb6250fd9817c084
shabloel <shabloel@outlook.com>
2023-07-10 23:46:57 +0200

    new code

--- FE/multiplication-game/src/app/app.module.ts
+++ FE/multiplication-game/src/app/app.module.ts
@@ -5,10 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChallengeComponentComponent } from './components/challenge-component/challenge-component.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, ChallengeComponentComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- FE/multiplication-game/src/app/components/challenge-component/challenge-component.component.ts
+++ FE/multiplication-game/src/app/components/challenge-component/challenge-component.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Challenge } from 'src/app/dtos/challenge';
 import { ChallengeAttemptDto } from 'src/app/dtos/challenge-attempt-dto';
 import { ChallengeServiceService } from 'src/app/services/challenge-service.service';
@@ -10,35 +15,58 @@ import { ChallengeServiceService } from 'src/app/services/challenge-service.serv
   styleUrls: ['./challenge-component.component.css'],
 })
 export class ChallengeComponentComponent implements OnInit {
-
   form: FormGroup;
-  
+  haveChallenge: boolean;
   challenge: Challenge = new Challenge();
+  loading = false;
+  success = false;
 
-  constructor(private challengeService: ChallengeServiceService) {}
+  constructor(
+    private challengeService: ChallengeServiceService,
+    private formBuilder: FormBuilder
+  ) {
+    this.haveChallenge = false;
+  }
 
   ngOnInit(): void {
+    this.formBuild();
     this.challengeService.getChallenge().subscribe(
       (result) => {
         this.challenge = result;
+        this.haveChallenge = true;
       },
       (error) => {
         console.error('Could not reach the server.');
       }
     );
-
-    this.formBuild();
   }
 
-  public submitChallenge(event: any) {
+  async submitChallenge() {
+    this.loading = true;
+    const formValue = this.form.value;
     const challengeAttempt = new ChallengeAttemptDto();
-    this.challengeService.sendChallenge(challengeAttempt);
+    challengeAttempt.factorA = this.challenge.factorA;
+    challengeAttempt.factorB = this.challenge.factorB;
+    challengeAttempt.guess = formValue.answer;
+    challengeAttempt.userAlias = formValue.name;
+
+    this.challengeService
+      .sendChallenge(challengeAttempt)
+      .subscribe((result) => {});
   }
 
   private formBuild() {
-    this.form = new FormGroup({
-      user: new FormControl('', Validators.compose([Validators.required, Validators.maxLength(12)])),
-      answer: new FormControl('', Validators.compose([Validators.required, value ]))
+    this.form = this.formBuilder.group({
+      alias: ['', [Validators.required, Validators.maxLength(12)]],
+      guess: ['', [Validators.required, Validators.min(0)]],
     });
+  }
+
+  get alias() {
+    return this.form.get('alias');
+  }
+
+  get guess() {
+    return this.form.get('guess');
   }
 }
