57487d30dbdea49359220a3e466c792a1025321a
shabloel <shabloel@outlook.com>
2023-09-23 00:24:07 +0200

    added tests for leaderboard

--- BE/gamification/src/test/java/com/games/gamification/gamification/services/LeaderBoardServiceTest.java
+++ BE/gamification/src/test/java/com/games/gamification/gamification/services/LeaderBoardServiceTest.java
@@ -1,5 +1,8 @@
 package com.games.gamification.gamification.services;
 
+import com.games.gamification.gamification.domain.model.BadgeCard;
+import com.games.gamification.gamification.domain.model.BadgeType;
+import com.games.gamification.gamification.domain.model.LeaderBoardRow;
 import com.games.gamification.gamification.repositories.BadgeRepo;
 import com.games.gamification.gamification.repositories.ScoreCardRepo;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class LeaderBoardServiceTest {
@@ -26,10 +36,16 @@ class LeaderBoardServiceTest {
     @Test
     void getCurrentLeaderBoard() {
         //given
-
+        LeaderBoardRow leaderBoardRow = new LeaderBoardRow(1L, 150L);
+        given(scoreCardRepo.findFirstTen()).willReturn(List.of(leaderBoardRow));
+        given(badgeRepo.findByUserIdOrderByBadgeTimestampDesc(anyLong()))
+                .willReturn(List.of(new BadgeCard(1L, BadgeType.BRONZE)));
+        List<LeaderBoardRow>  expectedLeaderBoardRows =
+                List.of(new LeaderBoardRow(1L, 150L, List.of(BadgeType.BRONZE.getDescription())));
         //when
-
+        List<LeaderBoardRow> result = classUnderTest.getCurrentLeaderBoard();
 
         //then
+        then(result).isEqualTo(expectedLeaderBoardRows);
     }
 }
