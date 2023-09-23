73459ded728afb3f79e98e4ab38199a8c7aec2de
shabloel <shabloel@outlook.com>
2023-09-23 11:11:42 +0200

    added configuration

---
+++ BE/gamification/src/main/java/com/games/gamification/gamification/configuration/JsonConfiguration.java
@@ -0,0 +1,13 @@
+package com.games.gamification.gamification.configuration;
+
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JsonConfiguration {
+    @Bean
+    public Hibernate5JakartaModule hibernateModule() {
+        return new Hibernate5JakartaModule();
+    }
+}
